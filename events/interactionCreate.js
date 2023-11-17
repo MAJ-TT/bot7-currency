b51c6c4da2f2dcc82f3a959e6cc6a689bbc9eda3
MAJ <zagzig186@gmail.com>
2023-11-17 16:25:52 -0400

    Added Cooldowns

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,5 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
+
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -13,6 +14,47 @@ module.exports = {
 		}
 
 		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+	},
+};
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+		const command = interaction.client.commands.get(interaction.commandName);
+		const { cooldowns } = interaction.client;
+		if (!command) {
+			console.error('No command found');
+			return;
+		}
+		if (!cooldowns.has(command.data.name)) {
+			cooldowns.set(command.data.name, new Collection());
+		}
+
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.data.name);
+		const defaultCooldownDuration = 5;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round((expirationTime - now) / 1000);
+				return interaction.reply({ content: `Please wait, you are on cooldown for \`${command.data.name}\` for \`${expiredTimestamp}\` seconds.`, ephemeral: true });
+			}
+		}
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+		try {
+			console.log('Command Executed');
 			await command.execute(interaction);
 		}
 		catch (error) {
