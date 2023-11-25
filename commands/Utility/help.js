0b37ac2dfa5dda5541a43db177959f27e666e4bc
MAJ <zagzig186@gmail.com>
2023-11-25 16:15:38 -0400

    help command Updated

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -1,28 +1,42 @@
-// help.js
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Show help menu'),
+		.setDescription('Shows help menu.'),
 
 	async execute(interaction) {
 		try {
 			const commandsPath = path.join(__dirname, '..', 'Utility');
 			const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-			const commandList = commandFiles.map(file => {
+			const commandList = [];
+			for (const file of commandFiles) {
 				const command = require(path.join(commandsPath, file));
-				return `/${command.data.name}: ${command.data.description}`;
-			});
+				const commandData = command.data;
 
-			await interaction.reply({ content: `Available commands:\n${commandList.join('\n')}`, ephemeral: true });
+				const description = commandData.description && commandData.description.trim() !== '' ? commandData.description : 'No description available';
+
+				const capitalizedDescription = description.charAt(0).toUpperCase() + description.slice(1);
+
+				// Format description in Discord block quotes
+				const formattedDescription = `> ${capitalizedDescription.replace(/\n/g, '\n> ')}`;
+
+				commandList.push(`**/${commandData.name}**:\n${formattedDescription}`);
+			}
+
+			const embed = new EmbedBuilder()
+				.setColor('#000001')
+				.setTitle('Help Menu')
+				.setDescription(commandList.join('\n\n'));
+
+			await interaction.reply({ embeds: [embed], ephemeral: false });
 		}
 		catch (error) {
 			console.error(error);
-			await interaction.reply({ content: 'There was an error while fetching commands.', ephemeral: true });
+			await interaction.reply({ content: 'There was an error while fetching commands.', ephemeral: false });
 		}
 	},
 };
